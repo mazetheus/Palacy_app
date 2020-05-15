503c6a347df73d99710060dc508718607e00139b
Matheus Azevedo <omatheusazevedo@gmail.com>
2020-05-15 01:14:55 -0300

    Adição de recursos
    Iframe, animações, ajuste de responsividade

---
+++ js/scripts2.js
@@ -0,0 +1,8 @@
+function upUp(){
+  var element = document.getElementById("logoholder");
+  element.classList.add("logoholderup");
+  element = document.getElementById("img-logo");
+  element.classList.add("img-logo-min");
+  element = document.getElementById("h2-agendamento");
+  element.classList.add("disabled");
+};
