d9341852bfb76154bfc0c21e76bdcd8698fb1acf
Matheus Azevedo <omatheusazevedo@gmail.com>
2020-05-19 03:06:13 -0300

    Junção de Abas, melhorias em agendar

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 
 function menuNavigation(evt, tabName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
