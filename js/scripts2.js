8324778014336ea5c17cb9c72d0c6f5e6de6866c
Matheus Azevedo <omatheusazevedo@gmail.com>
2020-06-14 17:12:10 -0300

    Revert "Agendar versão desktop e ajustes mobile"
    This reverts commit c08bc916d04546ce0fb4e17aef3c983f79b356c0.

--- js/scripts2.js
+++ js/scripts2.js
@@ -5,7 +5,4 @@ function upUp(){
   element.classList.add("img-logo-min");
   element = document.getElementById("h2-agendamento");
   element.classList.add("disabled");
-  element = document.getElementsById("top-itens");
-  console.log("top-itens");
-  element.classList.add("top-itens-small");
 };
