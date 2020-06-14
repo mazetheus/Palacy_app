c08bc916d04546ce0fb4e17aef3c983f79b356c0
Matheus Azevedo <omatheusazevedo@gmail.com>
2020-06-13 23:11:04 -0300

    Agendar versão desktop e ajustes mobile

--- js/scripts2.js
+++ js/scripts2.js
@@ -5,4 +5,7 @@ function upUp(){
   element.classList.add("img-logo-min");
   element = document.getElementById("h2-agendamento");
   element.classList.add("disabled");
+  element = document.getElementsById("top-itens");
+  console.log("top-itens");
+  element.classList.add("top-itens-small");
 };
